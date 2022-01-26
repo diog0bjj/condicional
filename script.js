829a68e3ab090108a686df2e513da0ac14c0ee60
DiogoSales07 <dsales289@poli.ufrj.br>
2022-01-26 16:04:02 -0300

    estudando condicional

---
+++ script.js
@@ -0,0 +1,25 @@
+let number = 19;
+if (number>18){
+    console.log("Seu número é maior")
+}
+
+let age2=20;
+let imhuman=true;
+if (age2>=18 && imhuman==true){
+    console.log("Você é maior de idade")
+}
+let birthday = "dezembro";
+if (birthday=="janeiro" || birthday=="dezembro"){
+    console.log("voce nasceu em janeiro ou dezembro")
+}
+
+let name = "Rodrigo";
+if (name[0]=="R"){
+    console.log("Seu nome começa com R")
+}
+
+let firstname = "Diogo";
+let lastname = "Nascimento"
+if(firstname[0]=="E" || lastname.length>6){
+    console.log("belo nome")
+}
